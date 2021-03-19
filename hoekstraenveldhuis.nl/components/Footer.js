12c11430c3a8ce997c9a996031a70fb2fa0d4d18
j.veldhuis <jobbfveldhuis@gmail.com>
2021-03-19 01:22:24 +0100

    Working CTA in NextJS app

--- hoekstraenveldhuis.nl/components/Footer.js
+++ hoekstraenveldhuis.nl/components/Footer.js
@@ -1,9 +1,38 @@
 import React from 'react'
 
-const Footer = props => (
-    <div>
+/**
+ * NextJS
+ */
+import Link from 'next/link'
 
+/**
+ * Styles
+ */
+import styles from '../styles/Footer.module.css'
+
+
+const Footer = props => (
+    <div className={styles.footerComponent}>
+        { props.showCTA && (
+          <Link href="/contact">
+              <a>
+                  <div className={`${styles.ctaContainer} container-fluid d-flex`} id="cta">
+                        <div className={styles.ctaContent}>
+                            <h2>Klaar voor de volgende stap?</h2>
+                            <p>Neem direct vrijblijvend contact op. We denken graag mee.</p>
+                        </div>
+                  </div>
+              </a>
+          </Link>
+        )}
+        <div className={styles.footerContainer}>
+
+        </div>
     </div>
 )
+
+Footer.defaultProps = {
+    'showCTA': true
+}
 
 export default Footer;
--- hoekstraenveldhuis.nl/pages/index.js
+++ hoekstraenveldhuis.nl/pages/index.js
@@ -8,11 +8,14 @@ export default function Home() {
       <Head>
         {/* Website meta data */}
         {/* TODO: Add OG and schema markup */}
-        <meta charset="UTF-8"/>
         <title>Hoekstra & Veldhuis - Samen vertellen we uw verhaal</title>
         <link rel="icon" type="image/png" href="../favicon.png" />
         <meta name="description"
               content="Bij Hoekstra & Veldhuis draait alles om tekst. Of het nu gaat om tekstschrijven, vertalen of SEO optimalisatie. U bent bij ons aan het juiste adres."/>
+
+              {/* Custom fonts */}
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+              <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Prata&display=swap" rel="stylesheet" />
       </Head>
 
       <Layout>
