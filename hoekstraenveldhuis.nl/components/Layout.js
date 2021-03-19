b36c459ab3921843ac2fe62635b0826af1f29f38
j.veldhuis <jobbfveldhuis@gmail.com>
2021-03-19 02:38:58 +0100

    Enforced ESLint style on Layout component

--- hoekstraenveldhuis.nl/components/Layout.js
+++ hoekstraenveldhuis.nl/components/Layout.js
@@ -7,20 +7,19 @@ import Header from './Header'
 import Footer from './Footer'
 
 const menu = [
-    {
-        'title': 'over ons',
-        'link': '/over-ons'
-    },
-    {
-        'title': 'onze boeken',
-        'link': '/onze-boeken'
-    },
-    {
-        'title': 'contact opnemen',
-        'link': '/contact'
-    }
+  {
+    title: 'over ons',
+    link: '/over-ons'
+  },
+  {
+    title: 'onze boeken',
+    link: '/onze-boeken'
+  },
+  {
+    title: 'contact opnemen',
+    link: '/contact'
+  }
 ]
-
 
 const Layout = props => (
     <div className="layout">
@@ -35,7 +34,7 @@ const Layout = props => (
 )
 
 Layout.defaultProps = {
-    showFooterCTA: true,
+  showFooterCTA: true
 }
 
-export default Layout;
+export default Layout
