a55c2c3e2a07567fa821fcd14bec37b2c9720e0d
j.veldhuis <jobbfveldhuis@gmail.com>
2021-03-21 14:48:12 +0100

    Fixed linting errors in _app.js

--- hoekstraenveldhuis.nl/pages/_app.js
+++ hoekstraenveldhuis.nl/pages/_app.js
@@ -1,4 +1,14 @@
 /**
+ * React
+ */
+import React from 'react'
+
+/**
+ * PropTypes
+ */
+import PropTypes from 'prop-types'
+
+/**
  * Bootstrap
  */
 import 'bootstrap/dist/css/bootstrap.css'
@@ -8,8 +18,13 @@ import 'bootstrap/dist/css/bootstrap.css'
  */
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp ({ Component, pageProps }) {
   return <Component {...pageProps} />
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.node.isRequired,
+  pageProps: PropTypes.node
 }
 
 export default MyApp
