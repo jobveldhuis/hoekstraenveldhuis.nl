f2cada2d198f0697ea42718a8dc25acfba40334b
j.veldhuis <jobbfveldhuis@gmail.com>
2021-03-21 15:09:32 +0100

    Conditional format links in navbar

--- hoekstraenveldhuis.nl/components/Header.js
+++ hoekstraenveldhuis.nl/components/Header.js
@@ -4,6 +4,7 @@ import React from 'react'
  * NextJS
  */
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 /**
  * PropTypes
@@ -15,42 +16,44 @@ import PropTypes from 'prop-types'
  */
 import styles from '../styles/Header.module.css'
 
-const Header = props => (
-    <div className={styles.navigation}>
-        <nav className={`${styles.navbar} navbar-expand-lg d-flex navbar-light`}>
+const Header = props => {
+  const router = useRouter()
+  return (
+        <div className={styles.navigation}>
+            <nav className={`${styles.navbar} navbar-expand-lg d-flex navbar-light`}>
         <span className={'navbar-brand'}>
             <Link href="/">
                 <a><img src="/images/henv_logo.png" className={styles.logo} alt="Logo Hoekstra & Veldhuis"/></a>
             </Link>
         </span>
-            <button className={`${styles.navbarToggler} ${styles.collapsed} navbar-toggler`} type="button"
-                    data-toggle="collapse" data-target="#navbar"
-                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className={`${styles.iconBar} ${styles.topBar}`}/>
-                <span className={`${styles.iconBar} ${styles.middleBar}`}/>
-                <span className={`${styles.iconBar} ${styles.bottomBar}`}/>
-            </button>
-            <div className={`collapse navbar-collapse ${styles.navbarCollapse} flex-grow-1`} id="navbar">
-                <ul className={`${styles.nav} navbar-nav justify-content-end flex-grow-1`}>
-                    {
-                        props.menu?.map((item, index) => {
-                          const classes = `${styles.navLink} nav-item`
-                          if (index === 0) classes.concat(' first')
-                          if (index === props.menu.length - 1) classes.concat(' last')
-                          return (
-                                <li className={classes} key={index}>
-                                    <Link href={item.link}>
-                                        <a>{item.title}</a>
-                                    </Link>
-                                </li>
-                          )
-                        })
-                    }
-                </ul>
-            </div>
-        </nav>
-    </div>
-)
+                <button className={`${styles.navbarToggler} ${styles.collapsed} navbar-toggler`} type="button"
+                        data-toggle="collapse" data-target="#navbar"
+                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className={`${styles.iconBar} ${styles.topBar}`}/>
+                    <span className={`${styles.iconBar} ${styles.middleBar}`}/>
+                    <span className={`${styles.iconBar} ${styles.bottomBar}`}/>
+                </button>
+                <div className={`collapse navbar-collapse ${styles.navbarCollapse} flex-grow-1`} id="navbar">
+                    <ul className={`${styles.nav} navbar-nav justify-content-end flex-grow-1`}>
+                        {
+                            props.menu?.map((item, index) => {
+                              let classes = `${styles.navLink} nav-item`
+                              if (item.link === router.pathname) classes = `${styles.navLink} ${styles.current} nav-item`
+                              return (
+                                    <li className={classes} key={index}>
+                                        <Link href={item.link}>
+                                            <a>{item.title}</a>
+                                        </Link>
+                                    </li>
+                              )
+                            })
+                        }
+                    </ul>
+                </div>
+            </nav>
+        </div>
+  )
+}
 
 /**
  * Default props for header
