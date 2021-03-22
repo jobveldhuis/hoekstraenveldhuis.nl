f82c9c1a01936ab3bb0bc2dca35547d5e3cf1410
j.veldhuis <jobbfveldhuis@gmail.com>
2021-03-22 23:02:55 +0100

    Created ScrollDownAnimation component

---
+++ hoekstraenveldhuis.nl/components/ScrollDownComponent.js
@@ -0,0 +1,60 @@
+/**
+ * React
+ */
+import React from 'react'
+
+/**
+ * PropTypes
+ */
+import PropTypes from 'prop-types'
+
+/**
+ * Lottie
+ */
+import { Player } from '@lottiefiles/react-lottie-player'
+
+/**
+ * Styling
+ */
+import styles from '../styles/ScrollDownComponent.module.css'
+
+const ScrollDownComponent = (props) => (
+    <div className={styles.scrollDownContainer}>
+        <div className={styles.scrollDownContent}>
+            { props.isMainTitle
+              ? (
+                    <h1>{props.title}</h1>
+                )
+              : (
+                    <h2>{props.title}</h2>
+                )
+            }
+            <p>{props.description}</p>
+            <div className={styles.scrollDownAnimationContainer}>
+                <a href={`#${props.anchor}`}>
+                    <Player
+                        autoplay
+                        loop
+                        src={'/other/scrollDownAnimation.json'}
+                    />
+                </a>
+            </div>
+        </div>
+    </div>
+)
+
+ScrollDownComponent.defaultProps = {
+  title: '',
+  description: '',
+  anchor: '',
+  isMainTitle: true
+}
+
+ScrollDownComponent.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  anchor: PropTypes.string,
+  isMainTitle: PropTypes.bool
+}
+
+export default ScrollDownComponent
