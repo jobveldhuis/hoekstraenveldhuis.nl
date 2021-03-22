e7c21823853252cd8f2bfeb16392f703df157de1
j.veldhuis <jobbfveldhuis@gmail.com>
2021-03-23 00:43:58 +0100

    Added working ContactBadgeComponent

--- src/components/ContactBadgeComponent.js
+++ src/components/ContactBadgeComponent.js
@@ -10,7 +10,15 @@ import styles from '../styles/ContactBadgeComponent.module.css'
 
 const ContactBadgeComponent = (props) => (
     <div className={styles.contactBadgeComponentContainer}>
-
+        <span className={styles.badgeText}>
+            Neem direct contact op
+        </span>
+        <div className={styles.badgeContainer}>
+            <a href='/contact'>
+                <img src={'/images/bubble.svg'} />
+            </a>
+        </div>
+        <img className={styles.badgeArrow} src={'/images/arrow.svg'} />
     </div>
 )
 
