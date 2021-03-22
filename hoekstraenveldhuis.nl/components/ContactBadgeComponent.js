6d73a5aef8116f0bb196332728c71c00848d7cb7
j.veldhuis <jobbfveldhuis@gmail.com>
2021-03-22 23:49:49 +0100

    Created skeleton for ContactBadgeComponent

---
+++ hoekstraenveldhuis.nl/components/ContactBadgeComponent.js
@@ -0,0 +1,21 @@
+/**
+ * React
+ */
+import React from 'react'
+
+/**
+ * Styles
+ */
+import styles from '../styles/ContactBadgeComponent.module.css'
+
+const ContactBadgeComponent = (props) => (
+    <div className={styles.contactBadgeComponentContainer}>
+
+    </div>
+)
+
+ContactBadgeComponent.defaultProps = {
+
+}
+
+export default ContactBadgeComponent
