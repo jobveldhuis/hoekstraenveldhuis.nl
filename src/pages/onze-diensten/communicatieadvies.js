07709087eddabaed4a93601b5ed4da2158db9bc5
j.veldhuis <jobbfveldhuis@gmail.com>
2021-03-23 00:56:01 +0100

    Added 'communicatieadvies' landing page

---
+++ src/pages/onze-diensten/communicatieadvies.js
@@ -0,0 +1,53 @@
+/**
+ * React
+ */
+import React from 'react'
+
+/**
+ * UI Components
+ */
+import LandingPage from '../../components/LandingPage'
+
+export default function CopyWriting () {
+  return (
+        <LandingPage
+            heading='Communicatieadvies op maat'
+            pageName='Communicatieadvies'
+            description='We adviseren u graag op het gebied van uw (bedrijfs)communicatie. Vrijblijvend kennismaken? Neem direct contact met ons op.'
+        >
+            <p style={{ fontWeight: 'bolder' }}>
+                In ieder bedrijf is communicatie ontzettend belangrijk. Zo kunnen belangrijke bedrijfsprocessen spaak lopen door een slechte interne communicatie of heeft het bedrijf moeite met het vinden van de juiste identiteit voor bijvoorbeeld nieuwsbrieven en posters, ook wel: externe communicatie. Vaak gaan de twee soorten communicatie hand in hand: wie zijn identiteit voor de buitenwereld wil veranderen, moet hiervoor de werkelijke identiteit van het bedrijf veranderen.
+            </p>
+
+            <h2>Interne communicatie</h2>
+            <p>
+                De interne communicatie is, in de breedste zin van het woord, het beschikbaar maken ontvangen en doorgeven van informatie tussen medewerkers in een organisatie. Denk hierbij bijvoorbeeld aan het informeren van medewerkers over voorgenomen besluiten door het MT of de werkwijze waarop er binnen het bedrijf gewerkt wordt.
+            </p>
+            <p>
+                Op deze manier zorg je ervoor dat je medewerkers goed op de hoogte zijn, hun werk goed kunnen doen en uiteindelijk zelf een belangrijk uithangbord voor je bedrijf kunnen zijn. Wanneer er sprake is van slechte interne communicatie, kan er een roddelcircuit op gang komen, waarbij verschillende geruchten en onwaarheden worden verspreid en waarbij medewerkers zich minder verantwoordelijk voelen voor het bedrijf en het af te leveren eindproduct.
+            </p>
+            <p>
+                Een onderzoek doen naar de interne communicatie en het in kaart brengen van de mogelijke verbeteringen kan duur en tijdrovend zijn. Daarom hebben we bij Hoekstra & Veldhuis een speciale quickscan ontwikkeld. Na ons eerste contact, komen we een dag op bezoek om u en uw medewerkers een aantal vragen te stellen over de manier waarop gecommuniceerd wordt. Ook onderzoeken we via welke kanalen er gecommuniceerd wordt en op welke manier dat gebeurt. Onze bevindingen delen we vervolgens met u in een eerste rapport, waarin we ook onze aanbevelingen doen. Met dit rapport kunt u vervolgens zelf bepalen óf en op welke manier u onze aanbevelingen wilt implementeren in uw bedrijf.
+            </p>
+
+            <h2>Externe communicatie</h2>
+            <p>
+                Onder externe communicatie valt elke vorm van communicatie die u met uw bedrijf doet naar de buitenwereld. Door middel van externe communicatie brengt u uw product of merk over aan de consument. Om zo effectief mogelijk te communiceren is het belangrijk om uw externe communicatie op elkaar af te stemmen. Indien u een post plaatst op Facebook, waarin u een USP, een Unique Selling Point, noemt van uw product, kunt u beter niet datzelfde USP  onderuithalen in een interview wat u op later moment geeft.
+            </p>
+            <p>
+                Externe communicatie ligt vaak bij verschillende afdelingen. Zo doet een klein gedeelte van de marketingafdeling bijvoorbeeld nieuwsbrieven, terwijl een ander gedeelte van de salesafdeling druk bezig is om actief contact te onderhouden met klanten door middel van extra calls en meetings. Door de interne communicatie te bevorderen, zal ook uw externe communicatie met sprongen vooruitgaan.
+            </p>
+
+            <h2>Waarom kiezen voor Hoekstra & Veldhuis?</h2>
+            <p>
+                We hebben ervaring met het opstellen en bijschaven van communicatieplannen, zowel bij kleinschalige startups, het MKB en overheidsinstellingen. Omdat communicatie vaak gaat om de eerlijkheid van de boodschap, komen we graag minstens één keer langs bij u op kantoor. Zo proeven we de sfeer en kijken we naar welke verbeteringen we op de korte en lange termijn kunnen doorvoeren om de positionering van uw product of merk nog sterker te maken.
+            </p>
+            <p>
+                Ook voor Engelstalige plannen kunt u bij ons terecht. We hebben medewerkers met affiniteit voor de Engelse taal én medewerkers die hun master in de Engelse taal en cultuur hebben behaald.
+            </p>
+            <p>
+                Zin om een keer een kop koffie met ons te drinken of om te sparren over de manier waarop we uw communicatie kunnen stroomlijnen? Neem direct vrijblijvend contact met ons op!
+            </p>
+        </LandingPage>
+  )
+}
